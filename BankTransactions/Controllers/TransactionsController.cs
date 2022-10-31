ea30c42f0e9f33a4f9655bc9c96060f741c2893c
abhay734 <vermaabhay734@gmail.com>
2022-10-31 10:19:32 +0530

    Making Transaction as default Controller

--- BankTransactions/Controllers/TransactionsController.cs
+++ BankTransactions/Controllers/TransactionsController.cs
@@ -48,7 +48,7 @@ namespace BankTransactions.Controllers
             return View(new Transaction());
         }
 
-        // POST: Transactions/Create
+        // POST: Transactions/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
