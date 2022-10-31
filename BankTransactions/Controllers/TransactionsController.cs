56aed35687b33a0a3e4bc3f413e2bcb64c829060
abhay734 <vermaabhay734@gmail.com>
2022-10-31 10:14:08 +0530

    Customizing error validation message

--- BankTransactions/Controllers/TransactionsController.cs
+++ BankTransactions/Controllers/TransactionsController.cs
@@ -53,7 +53,7 @@ namespace BankTransactions.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
         {
             if (ModelState.IsValid)
             {
--- BankTransactions/Models/Transaction.cs
+++ BankTransactions/Models/Transaction.cs
@@ -11,25 +11,27 @@ namespace BankTransactions.Models
 
         [Column(TypeName = "nvarchar(12)")]
         [DisplayName("Account Number")]
-        [Required]
+        [Required(ErrorMessage ="This field is required.")]
+        [MaxLength(12, ErrorMessage ="Maximum 12 characters only.")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
         [DisplayName("Beneficiary Name")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
         [DisplayName("Bank Name")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public string BankName { get; set; }
 
         [Column(TypeName = "nvarchar(11)")]
         [DisplayName("SWIFT Code")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
         public string SWIFTCode { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public int Amount { get; set; }
         public DateTime Date { get; set; }
     }
